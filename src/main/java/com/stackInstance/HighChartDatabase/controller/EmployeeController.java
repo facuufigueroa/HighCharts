ba866924652ab78ee830cf6c42c64aa9eb6805eb
facuufigueroa <figueroafacundo123@gmail.com>
2023-11-13 19:17:35 -0300

    Cambios finales en vista

--- src/main/java/com/stackInstance/HighChartDatabase/controller/EmployeeController.java
+++ src/main/java/com/stackInstance/HighChartDatabase/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package com.stackInstance.HighChartDatabase.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.stackInstance.HighChartDatabase.service.ICommentServiceImp;
 import com.stackInstance.HighChartDatabase.service.IUserServiceImp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class EmployeeController {
-	
 
+	@Autowired
+	private ICommentServiceImp commentServiceImp;
 
 	@Autowired
 	private IUserServiceImp userServiceImp;
 
-	@GetMapping("/barChart")
+	/*@GetMapping("/barChart")
 	public String getAllEmployee(Model model) {	
 		
 	List<String> username= userServiceImp.getAllUser().stream().map(x-> x.getName()).collect(Collectors.toList());
@@ -26,5 +28,34 @@ public class EmployeeController {
 	model.addAttribute("number_friends", numamigos);
 	return "barChart";
 	
+	}*/
+	@GetMapping("/home")
+	public String gettAllComments(Model model) {
+		int totalLikes = commentServiceImp.getAllComments().stream()
+				.mapToInt(x -> x.getNumber_likes())
+				.sum();
+
+		int totalDislikes = commentServiceImp.getAllComments().stream()
+				.mapToInt(x -> x.getNumber_dislikes())
+				.sum();
+		model.addAttribute("number_likes", totalLikes);
+		model.addAttribute("number_dislikes", totalDislikes);
+
+		List<String> username= userServiceImp.getAllUser().stream().map(x-> x.getName()).collect(Collectors.toList());
+		List<Integer> numamigos = userServiceImp.getAllUser().stream().map(x-> x.getNumber_friends()).collect(Collectors.toList());
+		model.addAttribute("name", username);
+		model.addAttribute("number_friends", numamigos);
+
+
+
+		return "home";
 	}
+
+	/*@GetMapping("/home")
+	public String getAllEmployee(Model model) {
+
+
+		return "home";
+
+	}*/
 }
